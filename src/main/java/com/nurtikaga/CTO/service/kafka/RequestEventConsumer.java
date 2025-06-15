be927171da5fb7aec25b5cc7ebf27f34c98cec0e
nurtikaga <147421319+nurtikaga@users.noreply.github.com>
2025-06-16 02:50:22 +0500

    added .env

--- src/main/java/com/nurtikaga/CTO/service/kafka/RequestEventConsumer.java
+++ src/main/java/com/nurtikaga/CTO/service/kafka/RequestEventConsumer.java
@@ -8,9 +8,9 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class RequestEventConsumer {
 
-    @KafkaListener(topics = "request.events", groupId = "sto-group")
+    @KafkaListener(topics = "request.events", groupId = "cto-group")
     public void handleRequestEvent(String message) {
         log.info("Получено событие: {}", message);
-        // Здесь можно добавить логику обработки событий
+
     }
 }
